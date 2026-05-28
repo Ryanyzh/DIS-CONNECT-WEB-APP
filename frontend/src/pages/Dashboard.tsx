985b0dbda94b7f0504c33db99fac2321d52964ed
denzelgoo <denzelgoo05@gmail.com>
2026-05-29 01:57:56 +0800

    Minor style changes

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -234,7 +234,7 @@ function Dashboard() {
       {/* Ticket Board */}
       <div className="bg-zinc-800/20 border border-zinc-700/20 rounded-xl m-4 h-full min-h-0 p-4">
         <div className="text-xl font-semibold mb-4">Ticket Board</div>
-        <div className="grid grid-cols-5 gap-4 justify-between h-full max-h-[79vh] min-h-0">
+        <div className="grid grid-cols-5 gap-4 justify-between h-full max-h-93/100 min-h-0">
           {columns.map(({ title, titleColor, filterFunc, bgColor }) => {
             const filteredTickets: TicketProps[] =
               tickets.filter(filterFunc);
